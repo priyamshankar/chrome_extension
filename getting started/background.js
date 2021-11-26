bb1be4e0bd668aa101e37146571e0af142551ec6
priyam <priyamshankar.5@gmail.com>
2021-11-27 02:37:56 +0530

    adding another timer extension for help

--- getting started/background.js
+++ getting started/background.js
@@ -7,11 +7,26 @@
 // });
 let color = '#3aa757';
 let brick = 0;
+let eyetim_value = 20 * 60;
+
 chrome.runtime.onStartup.addListener(() => {
-// chrome.runtime.onInstalled.addListener(() => {
+  // chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   chrome.storage.sync.set({ brick });
+  chrome.storage.sync.set({ eyetim_value });
+ 
 
 
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+  // console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+// function eyetimer() {
+//   chrome.storage.sync.get("eyetim_value", ({ eyetim_value }) => {
+//     var minutes = parseInt((eyetim_value) / 60);
+//     var seconds = ((eyetim_value) % 60);
+//     seconds = seconds < 10 ? '0' + seconds : seconds;
+//     eyeprot_output.innerHTML = `${minutes}:${seconds}`;
+//     eyetim_value--; // timervalue.value;
+//   })
+// }
+// eyeprot_output.innerHTML="helloworld";
--- getting started/popup.js
+++ getting started/popup.js
@@ -1,5 +1,6 @@
 // let color = '#3aa757';
 let changeColor = document.getElementById("changeColor");
+let click=document.getElementById("click");
 chrome.storage.sync.get("color", ({ color }) => {
   changeColor.style.backgroundColor = color;
 });
@@ -8,11 +9,13 @@ chrome.storage.sync.get("color", ({ color }) => {
 // When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
+  setInterval(eyetimer, 1000);
   chrome.scripting.executeScript({
     target: { tabId: tab.id, allFrames: true },
-    function: setPageBackgroundColor,
-    function: tick,                               // this script is perfectly working in the background
+    // function: setPageBackgroundColor,
+    // function: tick
+    // function: tick,                             // this script is perfectly working in the background
+  function: eye,
   });
   // alert("heyy there");
 });
@@ -24,23 +27,40 @@ function setPageBackgroundColor() {
   chrome.storage.sync.get("color", ({ color }) => {
     document.body.style.backgroundColor = color;
   });
-
+  
 }
-function tick() {
-  // function sick() {
-  // let brick = 0;
-  for (let i = 0; i < 5; i++) {
-    // chrome.storage.sync.set({ brick });
-    chrome.storage.sync.get("brick", ({ brick }) => {   
-
-      alert(brick);
-      brick++;
-      console.log(brick);
-      chrome.storage.sync.set({ brick });
-    });                // this script is perfectly working in the background 
+// function tick() {
+//   for (let i = 0; i < 5; i++) {
+//     // chrome.storage.sync.set({ brick });
+//     chrome.storage.sync.get("brick", ({ brick }) => {
       
-    }
+//       alert(brick);
+//       brick++;
+//       console.log(brick);
+//       chrome.storage.sync.set({ brick });
+//     });                // this script is perfectly working in the background 
+    
+//   }
+// }
+  
+let eyeprot_output=document.getElementById("eyeprot_output");
+function eye(){
 
 }
+click.addEventListener("click",(event)=>{
+  setInterval(eyetimer, 1000);
+
+  alert("message sent");
+})
+function eyetimer() {
+  // chrome.storage.sync.get("eyetim_value", ({ eyetim_value }) => {
+    var minutes = parseInt((eyetim_value) / 60);
+    var seconds = ((eyetim_value) % 60);
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    eyeprot_output.innerHTML = `${minutes}:${seconds}`;
+    eyetim_value--; // timervalue.value;
+  // })
+}
+// eyeprot_output.innerHTML="helloworld";
 // }
 
